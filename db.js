f9c1fe7d6ea3939c0a44dedf1e4bf0f20a19cc59
NasmaFanj29 <fanjnasma@gmail.com>
2026-04-11 21:35:56 +0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: "mysql-3d11ed7-fanjnasma-4923.a.aivencloud.com",
+  port: 26847,
+  user: "avnadmin",
+  password: process.env.DB_PASSWORD,// 👈 Check el-password el-7a2i2iye
+  database: "defaultdb",
+  ssl: { 
+    rejectUnauthorized: false // 👈 DARURE ktīr kirmal Aiven
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
