68084d59cc5fc702ffd88b3d6b33d4ad4c4baf8c
nasma29 <fanjnasma@gmail.com>
2026-04-11 21:13:47 +0300

    Initial backend commit

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -14,4 +15,22 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+=======
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: "mysql-3d11ed7-fanjnasma-4923.a.aivencloud.com",
+  port: 26847,
+  user: "avnadmin",
+  password: "AVNS_naebXmyEzZIlic1Xuaf", // 👈 Check el-password el-7a2i2iye
+  database: "defaultdb",
+  ssl: { 
+    rejectUnauthorized: false // 👈 DARURE ktīr kirmal Aiven
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+>>>>>>> 6e694a2 (Initial backend commit)
 module.exports = pool;
