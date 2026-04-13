67020e6670102ba4b01192e285a56ffc06e39418
nasma29 <fanjnasma@gmail.com>
2026-04-13 12:17:30 +0300

    fix db.js duplicate and conflicts

--- db.js
+++ db.js
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: "mysql-3d11ed7-fanjnasma-4923.a.aivencloud.com",
   port: 26847,
   user: "avnadmin",
-  password: process.env.DB_PASSWORD,// 👈 Check el-password el-7a2i2iye
+  password: process.env.DB_PASSWORD,
   database: "defaultdb",
-  ssl: { 
-    rejectUnauthorized: false // 👈 DARURE ktīr kirmal Aiven
+
+  ssl: {
+    rejectUnauthorized: false
   },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-=======
-const mysql = require('mysql2/promise');
-
-const pool = mysql.createPool({
-  host: "mysql-3d11ed7-fanjnasma-4923.a.aivencloud.com",
-  port: 26847,
-  user: "avnadmin",
-  password: process.env.DB_PASSWORD, // 👈 Check el-password el-7a2i2iye
-  database: "defaultdb",
-  ssl: { 
-    rejectUnauthorized: false // 👈 DARURE ktīr kirmal Aiven
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
->>>>>>> 6e694a2 (Initial backend commit)
 module.exports = pool;
