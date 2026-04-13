cca38a71434efe4a139cc21a2ea7e424e2d3f48c
nasma29 <fanjnasma@gmail.com>
2026-04-13 12:05:59 +0300

    add root route

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(cors({
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+
+app.get('/', (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
 /* --- 1. AUTHENTICATION --- */
 app.post('/api/admin/login', async (req, res) => {
   const { phone_number, password } = req.body;
